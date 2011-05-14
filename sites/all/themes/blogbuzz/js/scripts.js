b0c1c7acbe9133e870bd6370dcdeb6972e4e75e8
Joey Groh <joey@superstarmedia.com>
2011-05-13 21:11:38 -0700

    css tweaks

--- sites/all/themes/blogbuzz/js/scripts.js
+++ sites/all/themes/blogbuzz/js/scripts.js
@@ -7,6 +7,8 @@
     // add one of these lines for every year past so people cannot modify it
     $("#edit-field-child-vbssession-und-2010-summer").attr('disabled', 'disabled');
     $(".form-item-field-child-vbssession-und-2010-Summer label").addClass('past-vbs').html('<em>2010 Summer - this session has passed</em>');
+    
+    $('.field-name-field-message-audio').after("<div class='message-sub-text'>Click the play button to listen to the message or right click on the file name under the player and choose 'save target as' or 'download linked file as' to save it to your computer)</div>");
      
   });
 })(jQuery); 
