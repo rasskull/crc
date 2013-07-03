2694fc2ab65240739e34500db62bf28d110d9e89
Joey Groh <joey@superstarmedia.com>
2013-07-03 09:50:08 -0700

    js for old vbs past date

--- sites/all/themes/blogbuzz/js/scripts.js
+++ sites/all/themes/blogbuzz/js/scripts.js
@@ -12,6 +12,11 @@
     $("#edit-field-child-vbssession-und-2011-summer").attr('disabled', 'disabled');
     $(".form-item-field-child-vbssession-und-2011-Summer label").addClass('past-vbs').html('<em>2011 Summer - this session has passed</em>');
     
+    // 2012
+    $("#edit-field-child-vbssession-und-2012-summer").attr('disabled', 'disabled');
+    $(".form-item-field-child-vbssession-und-2012-Summer label").addClass('past-vbs').html('<em>2012 Summer - this session has passed</em>');
+    
+
     $('.field-name-field-message-audio').after("<div class='message-sub-text'>Click the play button to listen to the message or right click on the file name under the player and choose 'save target as' or 'download linked file as' to save it to your computer)</div>");
      
   });
